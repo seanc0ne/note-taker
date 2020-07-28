c287245bc06bae55d5a3a36106eb9d5fc477e271
Sean Cone <seandoesitbetter@gmail.com>
2020-07-28 10:05:02 -0700

    Updating server.js

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
