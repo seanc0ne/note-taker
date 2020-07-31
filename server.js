949fb1ef9ec920527bd2b901cf249e244cdd29f3
Sean Cone <seandoesitbetter@gmail.com>
2020-07-31 07:56:48 -0700

    Updating server.js

--- server.js
+++ server.js
@@ -5,14 +5,10 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
