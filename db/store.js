6ebeb212a82aee4eef57257b1337cc938976c5e1
Sean Cone <seandoesitbetter@gmail.com>
2020-07-31 08:27:31 -0700

    Adding store.js, writing functions

---
+++ db/store.js
@@ -0,0 +1,15 @@
+const util = require("util");
+const fs = require("fs");
+const uuidv1 = require('uuid/v1');
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+    read() {
+        return readFileAsync("db/db.json", "utf8");
+    }
+
+    write(note) {
+        return writeFileAsync("db/db.json")
+    }
+}
